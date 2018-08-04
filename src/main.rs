d7dbf36aaf8995e0dd2f5aca70f0208505647936
Jakub Arnold <darthdeus@gmail.com>
2018-08-05 00:32:20 +0200

    Working basic 2d grid

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,99 @@
-extern crate ggez;
+// #[macro_use]
+extern crate itertools;
+extern crate piston_window;
+extern crate graphics;
 
-use ggez::*;
-use ggez::graphics::{DrawMode, Point2, Rect};
+mod map;
+use map::*;
+use piston_window::*;
+use graphics::math::Matrix2d;
 
-struct Position {
-    x: u32,
-    y: u32
-}
+fn main() {
+    let screen_size = ScreenPoint::new(640f64, 480f64);
 
-struct Player {
-    position: Position,
-    name: String
-}
+    let mut window: PistonWindow =
+        WindowSettings::new("Hello Piston!", [screen_size.x as u32, screen_size.y as u32])
+            .exit_on_esc(true)
+            .build()
+            .unwrap();
+    let mut player_coord = Coord::new(0, 0);
 
-struct MainState {
-    pos_x: f32,
-    players: Vec<Player>
-}
+    while let Some(event) = window.next() {
+//        if let Some(ref args) = e.render_args() {
+//        }
 
-impl MainState {
-    fn new(_ctx: &mut Context) -> GameResult<MainState> {
-        let p1 = Player {
-            name: "john".to_string(),
-            position: Position { x: 3, y: 4 }
-        };
-        let s = MainState { pos_x: 0.0, players: vec![p1] };
-        Ok(s)
-    }
-}
+        if let Some(ref args) = event.press_args() {
+            use piston_window::Button::Keyboard;
+            use piston_window::Key;
 
-impl event::EventHandler for MainState {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
-        self.pos_x = self.pos_x % 800.0 + 2.0;
-        Ok(())
-    }
-
-    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        graphics::clear(ctx);
-
-        for x in 0..10 {
-            for y in 0..10 {
-                const grid_size: f32 = 20.0;
-
-                let rect = Rect::new((grid_size + 1f32) * x as f32, (grid_size + 1f32) * y as f32, grid_size, grid_size);
-                graphics::rectangle(ctx, DrawMode::Fill, rect);
+            if *args == Keyboard(Key::Left) {
+                player_coord.x -= 1;
             }
+
+            if *args == Keyboard(Key::Right) {
+                player_coord.x += 1;
+            }
+
+            if *args == Keyboard(Key::Up) {
+                player_coord.y -= 1;
+            }
+
+            if *args == Keyboard(Key::Down) {
+                player_coord.y += 1;
+            }
+//
+//            if *args == Keyboard(Key::Space) {
+//                self.board = Board::new(self.settings);
+//            }
+            // TODO: only update if necessary
+            // println!("update");
         }
-        graphics::circle(ctx,
-                         DrawMode::Fill,
-                         Point2::new(self.pos_x, 380.0),
-                         100.0,
-                         2.0)?;
-        graphics::present(ctx);
-        Ok(())
+        let mobs = vec![Coord::new(0, 1), Coord::new(-1, 3)];
+
+        let grid_size = 20.0f64;
+        let grid_spacing = 2.0f64;
+
+        let coord_to_screen_point = |coord: Coord| {
+            let rect_x: f64 = (coord.x as f64) * (grid_size + grid_spacing);
+            let rect_y: f64 = (coord.y as f64) * (grid_size + grid_spacing);
+
+            ScreenPoint::new(rect_x, rect_y)
+        };
+
+        window.draw_2d(&event, |context, graphics| {
+            clear([1.0; 4], graphics);
+
+            let player_point = coord_to_screen_point(player_coord);
+
+            let offset = -player_point + screen_size / 2;
+
+            let trans = graphics::math::multiply(graphics::math::rotate_radians(3.14 / 4.0),
+                                                 context.transform);
+
+
+            for x in -10..10 {
+                for y in -10..10 {
+                    let draw_point = coord_to_screen_point(Coord::new(x, y)) + offset;
+
+
+                    rectangle([0.5, 0.5, 0.5, 1.0],
+                              [draw_point.x, draw_point.y, grid_size, grid_size],
+                              trans,
+                              graphics);
+                }
+            }
+
+            let player_draw_at = player_point + offset;
+
+            rectangle([1.0, 0.5, 0.5, 1.0],
+                      [player_draw_at.x, player_draw_at.y, grid_size, grid_size],
+                      trans,
+                      graphics);
+
+            // rectangle([1.0, 0.0, 0.0, 1.0], // red
+            //           [0.0, 0.0, 100.0, 100.0],
+            //           context.transform,
+            //           graphics);
+        });
     }
 }
-
-pub fn main() {
-    let c = conf::Conf::new();
-    let ctx = &mut Context::load_from_conf("super_simple", "ggez", c).unwrap();
-    let state = &mut MainState::new(ctx).unwrap();
-    event::run(ctx, state).unwrap();
-}
-
---
+++ src/map.rs
@@ -0,0 +1,82 @@
+use std::ops::{Add, Div, Neg};
+
+#[derive(Debug, Copy, Clone)]
+enum MapItem {
+    Empty,
+    Tree,
+}
+
+struct GameMap {
+    size: Coord,
+    grid: Vec<MapItem>,
+}
+
+impl GameMap {
+    fn new(size: Coord) -> GameMap {
+        let item_count = (size.x * size.y) as usize;
+        let mut grid = Vec::with_capacity(item_count);
+        grid.resize(item_count, MapItem::Empty);
+        GameMap { size, grid }
+    }
+
+    fn at(&self, point: Coord) -> MapItem {
+        let idx = (point.x + point.y * self.size.x) as usize;
+        self.grid[idx]
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct ScreenPoint {
+    pub x: f64,
+    pub y: f64,
+}
+
+impl ScreenPoint {
+    pub fn new(x: f64, y: f64) -> ScreenPoint {
+        ScreenPoint { x, y }
+    }
+}
+
+impl Add for ScreenPoint {
+    type Output = ScreenPoint;
+
+    fn add(self, rhs: ScreenPoint) -> ScreenPoint {
+        ScreenPoint::new(self.x + rhs.x, self.y + rhs.y)
+    }
+}
+
+impl Div<i32> for ScreenPoint {
+    type Output = ScreenPoint;
+
+    fn div(self, rhs: i32) -> ScreenPoint {
+        ScreenPoint::new(self.x / rhs as f64, self.y / rhs as f64)
+    }
+}
+
+impl Neg for ScreenPoint {
+    type Output = ScreenPoint;
+
+    fn neg(self) -> ScreenPoint {
+        ScreenPoint::new(-self.x, -self.y)
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Coord {
+    pub x: i32,
+    pub y: i32,
+}
+
+impl Coord {
+    pub fn new(x: i32, y: i32) -> Coord {
+        Coord { x, y }
+    }
+}
+
+impl Add for Coord {
+    type Output = Coord;
+
+    fn add(self, other: Coord) -> Coord {
+        Coord::new(self.x + other.x, self.y + other.y)
+    }
+}
